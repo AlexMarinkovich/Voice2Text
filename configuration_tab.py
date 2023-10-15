e3f99dcfa31c3c038d8a7c8e525123bf817164d4
Alexander G Marinkovich <alex-989@live.com>
2023-10-15 09:00:48 -0400

    Optimized row deletion logic, fixed ui glitches

--- configuration_tab.py
+++ configuration_tab.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
 import json
 
 from relative_sizes import VW
@@ -16,20 +17,22 @@ class ConfigurationTab(QWidget):
         self.table_widget.setHorizontalHeaderLabels(["Phrase", "Replacement", ""])
         
         # Adjust the sizing of the table
-        self.table_widget.setColumnWidth(0, 15.5*VW)
-        self.table_widget.setColumnWidth(1, 15.5*VW)
+        self.table_widget.verticalHeader().setFixedWidth(1.2*VW)
+        self.table_widget.setColumnWidth(0, 15*VW)
+        self.table_widget.setColumnWidth(1, 15*VW)
         self.table_widget.setColumnWidth(2, 2.5*VW)
 
         # Make the headers non-selectable
         self.table_widget.horizontalHeader().sectionPressed.disconnect()
         self.table_widget.verticalHeader().sectionPressed.disconnect()
+        
+        # Set the table to have no horizontal scrolling
+        self.table_widget.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
         self.add_entry_button = QPushButton("Add New Phrase")
         self.layout.addWidget(self.add_entry_button)
         self.add_entry_button.setFixedHeight(2*VW)
         self.add_entry_button.clicked.connect(self.add_entry)
-        
-        self.delete_buttons = []
 
         # Save the configuration to the json file when the table is edited
         self.table_widget.itemChanged.connect(self.save_config)
@@ -38,26 +41,29 @@ class ConfigurationTab(QWidget):
         self.load_config()
 
     def add_entry(self, phrase="", replacement=""):
-        row_position = self.table_widget.rowCount()
-        self.table_widget.insertRow(row_position)
-        self.table_widget.setItem(row_position, 0, QTableWidgetItem(phrase))
-        self.table_widget.setItem(row_position, 1, QTableWidgetItem(replacement))
+        row_number = self.table_widget.rowCount()
+        self.table_widget.insertRow(row_number)
+        self.table_widget.setItem(row_number, 0, QTableWidgetItem(phrase))
+        self.table_widget.setItem(row_number, 1, QTableWidgetItem(replacement))
         
         # Create a delete button and connect it to the delete_entry method
         delete_button = QPushButton("✖")
-        delete_button.clicked.connect(lambda _, row=row_position: self.delete_entry(row))
-        self.table_widget.setCellWidget(row_position, 2, delete_button)
-        self.delete_buttons.append(delete_button)
+        delete_button.clicked.connect(self.delete_entry)
+        self.table_widget.setCellWidget(row_number, 2, delete_button)
 
-    def delete_entry(self, row: int):
-        self.table_widget.removeRow(row)
-        del self.delete_buttons[row]
+        # Automically scroll down
+        self.table_widget.verticalScrollBar().setValue(self.table_widget.verticalScrollBar().maximum())
 
-        # Update the row numbers in the delete buttons
-        for i, button in enumerate(self.delete_buttons):
-            button.clicked.disconnect()  # Disconnect the old signal
-            button.clicked.connect(lambda _, row=i: self.delete_entry(row))
+    def delete_entry(self):
+        selected_indexes = self.table_widget.selectedIndexes()
+        if not selected_indexes: return
+
+        row_number = selected_indexes[0].row()
+        self.table_widget.removeRow(row_number)
         
+        # Automically scroll down
+        self.table_widget.verticalScrollBar().setValue(self.table_widget.verticalScrollBar().maximum())
+
         # Save the configuration to the json file when an entry is deleted
         self.save_config()
 
